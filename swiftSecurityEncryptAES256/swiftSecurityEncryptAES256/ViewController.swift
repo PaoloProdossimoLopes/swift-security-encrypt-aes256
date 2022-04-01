9add36df7c3479c1e6e75264d69520432794fca5
Paolo Prodossimo Lopes <paolo.prodossimo.lopes@gmail.com>
2022-04-01 20:54:52 -0300

    Feature (App): Create app

---
+++ swiftSecurityEncryptAES256/swiftSecurityEncryptAES256/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swiftSecurityEncryptAES256
+//
+//  Created by Paolo Prodossimo Lopes on 01/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
